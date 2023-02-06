8debb4f8dae7de95e4071d05295bf1078d084f83
VerneriL <44767@edu.raseko.fi>
2023-02-06 15:12:31 +0200

    Postaus sivu

--- blogi/views.py
+++ blogi/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Postaus
+
+
+def postaukset(request):
+    postaukset = Postaus.objects.all()
+    context = {
+        "postaukset": postaukset
+    }
+    return render(request, "blogi/postauslista.html", context)
+
+def nayta_postaus(request, id):
+    return render(request, "blogi/postaus.html")
+    
