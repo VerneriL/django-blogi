d63d96a98c12c23d665cfc8822831efafca6c806
VerneriL <44767@edu.raseko.fi>
2023-02-13 15:01:49 +0200

    uusi_postaus lomake

--- blogi/views.py
+++ blogi/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from .models import Postaus
 
@@ -11,5 +12,20 @@ def postaukset(request):
     return render(request, "blogi/postauslista.html", context)
 
 def nayta_postaus(request, id):
-    return render(request, "blogi/postaus.html")
+    postaus = Postaus.objects.get(id=id)
+    context = {
+        "postaus": postaus
+    }
+    return render(request, "blogi/postaus.html", context)
+
+def uusi_postaus(request):
+    if request.method == "GET":
+        return render(request, "blogi/uusi_postaus.html")
+    elif request.method == "POST":
+        otsikko = request.POST['otsikko']
+        teksti = request.POST['teksti']
+        postaus = Postaus.objects.create(
+            otsikko=otsikko, 
+            teksti=teksti)
+        return redirect(reverse('nayta_postaus', args=(postaus.id,)))
     
--- koodausblogi/urls.py
+++ koodausblogi/urls.py
@@ -19,6 +19,7 @@ from blogi import views as blogi_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('postaukset/', blogi_views.postaukset),
-    path('postaus/<int:id>', blogi_views.nayta_postaus)
+    path('', blogi_views.postaukset, name="postauslista"),
+    path('postaus/<int:id>', blogi_views.nayta_postaus, name="nayta_postaus"),
+    path('postaus/uusi', blogi_views.uusi_postaus, name="uusi_postaus")
 ]
