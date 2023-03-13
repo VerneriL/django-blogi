0450ca5a8fffca63a7ed2db141bd7aa17e974f77
VerneriL <44767@edu.raseko.fi>
2023-03-13 13:38:42 +0200

    create_superuser

---
+++ create_admin_user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+
+import django
+
+from django.contrib.auth import get_user_model
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koodausblogi.settings')
+    django.setup()
+
+    username=os.environ.get('ADMIN_USER_NAME', 'admin')
+    email=os.environ.get('ADMIN_USER_EMAIL', '')
+    password=os.environ['ADMIN_USER_PASSWORD']
+
+    print(f"Creating superuser named {username!r}")
+    user_model = get_user_model()
+    user_model.objects.create_superuser(username=username, email=email, password=password)
+
+if __name__=='__main__':
+    main()
