b4ec7be5957273f42ca0c09580b0fd6be0a61e95
VerneriL <44767@edu.raseko.fi>
2023-03-20 09:39:43 +0200

    Ei yritä luoda adminia uudestaan

--- create_admin_user.py
+++ create_admin_user.py
@@ -12,14 +12,20 @@ def main():
 
     username=os.environ.get('ADMIN_USER_NAME', 'admin')
     email=os.environ.get('ADMIN_USER_EMAIL', '')
-    password=os.environ['ADMIN_USER_PASSWORD']
+    password=os.environ.get('ADMIN_USER_PASSWORD', '')
 
-    if username:
+    users =  get_user_model().objects
+
+    if users.filter(username=username).exists():
+        print(f"User {username!r} already exists")
         return
-    else:
-        print(f"Creating superuser named {username!r}")
-        user_model = get_user_model()
-        user_model.objects.create_superuser(username=username, email=email, password=password)
+    
+    if not password:
+        raise SystemExit("ADMIN_USER_PASSWORD environment variable is needed!")
+
+    print(f"Creating superuser named {username!r}")
+    user_model = get_user_model()
+    user_model.objects.create_superuser(username=username, email=email, password=password)
 
 if __name__=='__main__':
     main()
